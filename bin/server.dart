f915992eaf27016dc3a309705f97d4afb8589654
Shawn Von <Shawn Von>
2017-11-30 14:25:19 +0800

    服务器数据服务案例-cors未全部完成

--- bin/server.dart
+++ bin/server.dart
@@ -1,10 +1,37 @@
 import 'package:redstone/redstone.dart' as app;
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'package:shelf_cors/shelf_cors.dart' as shelf_cors;
 
-@app.Route("/head/")
-helloWorld() => "Hello, World! dart server";
+@app.Route("/data/")
+helloWorld() {
+  return getDataFromDB();
+}
+
+@app.Route("/data/add", methods: const [app.POST])
+addUser(@app.Body(app.JSON) Map user) {
+  var data = app.JSON;
+  
+  return app.JSON.toString() + "got 1youcsfffer data";
+}
+
+String getDataFromDB() {
+  //todo get data from db.
+  return '''["name1","name2","name33","name43"]''';
+}
+
 @app.Route("/register/")
-register()=>"you are now a member";
+register() => "you are now a member";
 main() {
+  Map corsHeaders1 = {
+    "ContentType": "application/json",
+    "Access-Control-Allow-Methods": "POST",
+    "Access-Control-Allow-Origin": "*"
+  };
+  shelf.Middleware middleware =
+      shelf_cors.createCorsHeadersMiddleware(corsHeaders: corsHeaders1);
   app.setupConsoleLog();
-  app.start();
+  app.addShelfMiddleware(middleware);
+  app.start(port: 90);
+  print("fefef");
 }
--- web/index.dart
+++ web/index.dart
@@ -1,18 +1,49 @@
+// Copyright (c) 2012, the Dart project authors.
+// Please see the AUTHORS file for details.
+// All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+import 'dart:convert';
 import 'dart:html';
-import 'dart:math' as math;
-void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen(reverseText);
+import 'package:http/browser_client.dart';
+
+var wordList;
+
+ main() async{
+  querySelector('#getData').onClick.listen(makeRequest);
+  
+  wordList = querySelector('#wordList');
+
+  var client = new BrowserClient();
+  var url = 'http://localhost:90/data/add';
+  var response =
+      await client.post(url, body: {'name': 'doodle', 'color': 'blue'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+  
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
+Future makeRequest(Event e) async {
+  var path='http://localhost:90/data/';
+  try {
+    processString(await HttpRequest.getString(path));
+  } catch (e) {
+    print('Couldn\'t open $path');
+    handleError(e);
   }
-  querySelector('#sample_text_id').text = buffer.toString();
-  //我做了点修改
-  querySelector("#sample_text_id").text="new text";
 }
+
+void processString(String jsonString) {
+  List<String> portmanteaux = JSON.decode(jsonString) as List<String>;
+  for (int i = 0; i < portmanteaux.length; i++) {
+    wordList.children.add(new LIElement()..text = portmanteaux[i]);
+  }
+}
+
+void handleError(Object error) {
+  wordList.children.add(new LIElement()..text = 'Request failed.');
+}
+
+
